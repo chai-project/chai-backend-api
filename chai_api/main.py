915acf66829f95740c7942bbe4d14ea9e489b13f
Kim Bauters <kim.bauters@bristol.ac.uk>
2022-07-07 10:51:54 +0100

    add sink to catch all requests

--- chai_api/main.py
+++ chai_api/main.py
@@ -18,6 +18,14 @@ from chai_persistence import Homes
 SCRIPT_PATH: str = os.path.dirname(os.path.realpath(__file__))
 config: Configuration = read_config(SCRIPT_PATH)
 
+
+class Sink:
+    def on_get(self, req: Request, resp: Response):  # noqa
+        resp.content_type = falcon.MEDIA_TEXT
+        resp.status = falcon.HTTP_NOT_FOUND
+        resp.text = "unknown API endpoint - make sure you did not omit the trailing slash"
+
+
 # start the Homes singleton
 homes = Homes()
 
@@ -29,6 +37,7 @@ app.add_route("/heating/mode/", HeatingResource())
 app.add_route("/battery/mode/", BatteryResource())
 app.add_route("/electricity/prices/", PriceResource())
 app.add_route("/electricity/current/", CurrentResource())
+app.add_sink(Sink().on_get, prefix="/")  # route all unknown traffic to the sink
 
 print(f"backend server running at {config.host}:{config.port}")
 bjoern.run(app, config.host, config.port, reuse_port=False)
